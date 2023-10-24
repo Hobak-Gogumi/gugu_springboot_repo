b5f90d335c97d8b1e3f1f13fe4a2129761d2acc9
SEOHYEON LEE <seoooohs2@naver.com>
2023-10-24 20:34:53 +0900

    1.2.4 엔티티 클래스와 JpaRepository

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -24,6 +25,21 @@ public class MemoRepositoryTests {
             Memo memo = Memo.builder().memoText("Sample..." + i).build();
             memoRepository.save(memo);
         });
+    }
+
+    @Test
+    public void testSelect(){
+        //데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("================================");
+
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
     }
 
 }
