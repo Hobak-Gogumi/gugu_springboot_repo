9999817fb3f57bc431290088e068d6873a563327
SEOHYEON LEE <seoooohs2@naver.com>
2023-10-27 14:40:27 +0900

    1.2.5 페이징/정렬 처리하기

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,9 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.zerock.ex2.entity.Memo;
 
 import java.util.Optional;
@@ -54,5 +57,16 @@ public class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault(){
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
     }
 }
